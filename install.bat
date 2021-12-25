f08fa28c98f2ad62094c09f2eccfb55827cfeef9
Duwa <94223854+DuwaYT@users.noreply.github.com>
2021-12-25 16:26:10 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+pip install threading
+pip install queue
+pip install requests
+pip install time
+pip install json
+pip install gratient
+pip install pyfade
+pip install colorama
+pip install fade
